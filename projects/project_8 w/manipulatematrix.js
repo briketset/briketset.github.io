4339d562629cb9151b7bb9f59336057a0dab8ce6
BriKet <88550982+briketset@users.noreply.github.com>
2023-07-23 20:52:11 +0700

    Create manipulatematrix.js

---
+++ projects/project_8 w/manipulatematrix.js
@@ -0,0 +1,28 @@
+"use strict";
+
+export class ManipulateMatrix {
+    constructor() {
+        
+        }
+        add() {
+            
+        }
+        multiple(a = new Array(2), b = []) {
+            let rowsA = a.length
+            let colsA = a[0].length
+            let rowsB = b.length
+            let colsB = b[0].length
+            let c = [];
+            if (colsA != rowsB) return false;
+            for (var i = 0; i < rowsA; i++) c[ i ] = [];
+            for (var k = 0; k < colsB; k++) {
+                for (var i = 0; i < rowsA; i++) {
+                    var t = 0;
+                    for (var j = 0; j < rowsB; j++) t += a[ i ][j]*b[j][k];
+                    c[ i ][k] = t;
+                }
+            }
+            return c;
+
+        }
+}        
